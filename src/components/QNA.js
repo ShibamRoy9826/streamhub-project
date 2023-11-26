adb29fd7e4841d40836d6b7517ee57b4db3e2045
Shibam Roy <royshibam9826@gmail.com>
2023-11-26 22:43:27 +0530

    Update QNA.js
    Made minor changes

--- src/components/QNA.js
+++ src/components/QNA.js
@@ -3,9 +3,9 @@ import React from 'react'
 const QNA=(props)=>{
 	return(
 	<>
-		<div className="flex flex-row items-center justify-center shadow-md">
+		<div className="flex flex-row items-center justify-center shadow-md question hover:my-2">
         <h2 className="m-2 text-center"><b>{props.question}</b></h2>
-        <button className="text-black bg-[#FB5008] rounded-lg w-12 h-full"><b>+</b></button>
+        <button className="text-black bg-[#FB5008] rounded-lg w-12 h-full qnaButton"><b>+</b></button>
         </div>
 
         <div className="bg-white hidden">
